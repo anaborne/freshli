9f524b0852626951fc3b181e9a5a8441f81b8d83
Immanuel Anaborne <anaborne@sas.upenn.edu>
2025-04-23 21:23:42 -0400

    chore: secured supabase credentials

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lfhkwoykslzcebyrzgli.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmaGt3b3lrc2x6Y2VieXJ6Z2xpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNjgyODgsImV4cCI6MjA2MDg0NDI4OH0.A2SqJ9B8Xz4cScqOv9aW16Db7B7VSH_H7rwjZDcaIXA';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
